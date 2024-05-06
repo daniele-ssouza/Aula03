776552ff4e1c8c02a916d9b35f9d0466dfea2950
daniele-ssouza <166664849+daniele-ssouza@users.noreply.github.com>
2024-05-06 20:37:04 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+df = pd.read_csv("https://raw.githubusercontent.com/hermeson883/data_science_workshop/main/linguagens.csv")
+
+#grafico = px.pie(df,names='Tipagem' )
+
+grafico = px.bar(df, x="Linguagem", y="Desenvolvedores", color='Linguagem' )
+st.plotly_chart(grafico)
+
+#st.bar_chart(df, x="Linguagem", y="Desenvolvedores")
